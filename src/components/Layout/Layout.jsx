70a7e363cc158f9a8a398e464517654eae644a46
Madugu Kelly <blaqj23@gmail.com>
2026-06-20 18:29:55 +0100

    fix: add default export to Layout component

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './Navbar';
+import LeftPanel from './LeftPanel';
+import RightPanel from './RightPanel';
+
+function Layout({ children, onToggleSidebar }) {
+  return (
+    <div className="flex flex-col h-screen">
+      <Navbar onToggleSidebar={onToggleSidebar} />
+      <div className="flex flex-1 overflow-hidden">
+        <LeftPanel />
+        <main className="flex-1 overflow-y-auto p-4">{children}</main>
+        <RightPanel />
+      </div>
+    </div>
+  );
+}
+export default Layout;
